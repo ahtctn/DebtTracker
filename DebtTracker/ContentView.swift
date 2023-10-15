b072e45c4e9226a9034f81523096a85231ec6f62
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-10-15 15:12:22 +0300

    Initial Commit

---
+++ DebtTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DebtTracker
+//
+//  Created by Ahmet Ali ÇETİN on 15.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DebtTracker/DebtTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebtTrackerApp.swift
+//  DebtTracker
+//
+//  Created by Ahmet Ali ÇETİN on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DebtTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
