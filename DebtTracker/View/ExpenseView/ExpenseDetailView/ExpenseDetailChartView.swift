4637fa89e035ba69a83db31df569b7a0da0d1c7a
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-10-17 18:19:58 +0300

    #FINAL COMMIT# iOS 17 is not availible in the project so i have to transfer to the new SwiftUI project and continue my developments

--- DebtTracker/View/ExpenseView/ExpenseDetailView/ExpenseDetailChartView.swift
+++ DebtTracker/View/ExpenseView/ExpenseDetailView/ExpenseDetailChartView.swift
@@ -6,38 +6,15 @@
 //
 
 import SwiftUI
-import Charts
 
 struct ExpenseDetailChartView: View {
-    var expense: ExpenseModel
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
-            Text("Chart")
-            Canvas { remainingDebt, paidDebt in
-                let total = slices.reduce(0) { $0 + $1.0 }
-                remainingDebt.translateBy(x: paidDebt.width * 0.5, y: paidDebt.height * 0.5)
-                var pieContext = remainingDebt
-                pie.rotate(by: .degrees(-90))
-                let radius = min(paidDebt.width, paidDebt.height) * 0.48
-                var startAngle = Angle.zero
-                for (value, color) in slices {
-                    let angle = Angle(degrees: 360 * (value / total))
-                    let endAngle = startAngle + angle
-                    let path = Path { p in
-                        p.move(to: .zero)
-                        p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
-                        p.closeSubpath()
-                    }
-                    pieContext.fill(path, with: .color(color))
-                    startAngle = endAngle
-                    
-                }
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-    
-    struct ExpenseDetailChartView_Previews: PreviewProvider {
-        static var previews: some View {
-            ExpenseDetailChartView(expense: ExpenseModel(title: "Kumar Borcu", subtitle: "Ekrem abiye kumar borcu", debtImage: Constants.TabBarImages.expense, totalDebt: 10_000, remainingDebt: 9_900, paymentPeriod: .monthly, paymentPlan: 4))
-        }
+}
+
+struct ExpenseDetailChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseDetailChartView()
     }
+}
---
+++ DebtTracker/View/ExpenseView/ExpenseDetailView/ExpenseDetailPieChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseDetailPieChartView.swift
+//  DebtTracker
+//
+//  Created by Ahmet Ali ÇETİN on 17.10.2023.
+//
+
+import SwiftUI
+
+struct ExpenseDetailPieChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseDetailPieChartView()
+}
